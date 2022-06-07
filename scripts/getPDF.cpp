3a57ecfcf8724072338bdac6de2d25ff038e87a2
Jamicus96 <jepage999@gmail.com>
2022-06-06 22:11:07 -0400

    bug fixing

--- scripts/getPDF.cpp
+++ scripts/getPDF.cpp
@@ -205,18 +205,19 @@ TH1D* PlotHitTimeResidualsMCPosition(const std::string& fileName, std::vector<do
     if (verbose) {std::cout << "Looping through entries..." << std::endl;}
     unsigned int evt_idx = 0;
     unsigned int num_evts = 0;
-    for( size_t iEntry = 0; iEntry < dsReader.GetEntryCount(); iEntry++ ) {
+    for(size_t iEntry = 0; iEntry < dsReader.GetEntryCount(); iEntry++) {
         const RAT::DS::Entry& rDS = dsReader.GetEntry( iEntry );
         const TVector3 eventPosition = rDS.GetMC().GetMCParticle(0).GetPosition(); // At least 1 is somewhat guaranteed
-        for( size_t iEV = 0; iEV < rDS.GetEVCount(); iEV++ ) {
+        for(size_t iEV = 0; iEV < rDS.GetEVCount(); iEV++) {
             const RAT::DS::EV& rEV = rDS.GetEV( iEV );
             const RAT::DS::CalPMTs& calibratedPMTs = rEV.GetCalPMTs();
-            for( size_t iPMT = 0; iPMT < calibratedPMTs.GetCount(); iPMT++ ) {
+            for(size_t iPMT = 0; iPMT < calibratedPMTs.GetCount(); iPMT++) {
                 if (is_oPs && delays.at(evt_idx) == 0.0) {  // Filter out non o-Ps events
                     continue;
-                    std::cout << "RIP";
+                    std::cout << "RIP" << std::endl;
                 } else {
                     ++num_evts;
+                    std::cout << "YASS" << std::endl;
                     const RAT::DS::PMTCal& pmtCal = calibratedPMTs.GetPMT(iPMT);
 
                     // lightPath.CalcByPosition( eventPosition, pmtInfo.GetPosition( pmtCal.GetID() ) );
