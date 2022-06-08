70a89f5ad901acc5803444dfdb711cb6f3000cea
Jamicus96 <jepage999@gmail.com>
2022-06-08 16:01:31 -0400

    debugging

--- scripts/ClassifierResults.cpp
+++ scripts/ClassifierResults.cpp
@@ -150,7 +150,7 @@ std::vector<std::vector<double> > findPositronDelays_andClassification(const std
             const RAT::DS::CalPMTs& calibratedPMTs = rEV.GetCalPMTs();
             //std::cout << "2" << std::endl;
             const TVector3 eventPosition = rDS.GetMC().GetMCParticle(0).GetPosition(); // At least 1 is somewhat guaranteed
-            double event_time = 390 - rDS.GetMCEV().GetGTTime();  // event time is 390ns - GT time.
+            double event_time = 390 - rDS.GetMCEV(0).GetGTTime();  // event time is 390ns - GT time.
             //std::cout << "3" << std::endl;
             for(size_t iPMT = 0; iPMT < calibratedPMTs.GetCount(); iPMT++) {
                 //std::cout << "4" << std::endl;
